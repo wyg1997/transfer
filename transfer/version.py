1f5f91ed4205fb18d4e6fec83a2275937df12370
wyg1997 <wyg19970408@gmail.com>
2022-05-18 20:09:53 +0800

    :tada: refine version update

--- transfer/version.py
+++ transfer/version.py
@@ -1,9 +1,6 @@
 try:
-    from ._version import __version__
-except ImportError:
-    try:
-        from setuptools_scm import get_version
+    from setuptools_scm import get_version
 
-        __version__ = get_version(root="..", relative_to=__file__)
-    except (ImportError, LookupError):
-        __version__ = "UNKNOWN"
+    __version__ = get_version(root="..", relative_to=__file__)
+except (ImportError, LookupError):
+    __version__ = "UNKNOWN"
